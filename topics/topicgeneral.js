148c942739f3a28c5152f8a42e94a66a8832d7fe
Davide Scapellato <irondavide06@gmail.com>
2025-10-30 15:04:09 +0100

    new index

---
+++ topics/topicgeneral.js
@@ -0,0 +1,31 @@
+divs=document.getElementsByClassName("exit")
+for(let i=0; i<divs.length; i++){
+     divs[i].onclick=function(){
+         window.location.href="../topics.html"
+    }
+}
+
+divs=document.getElementsByClassName("one")
+for(let i=0; i<divs.length; i++){
+    divs[i].onclick=function(){
+        window.location.href="subtopic_1/first.html"
+    }
+}
+divs=document.getElementsByClassName("two")
+for(let i=0; i<divs.length; i++){
+    divs[i].onclick=function(){
+        window.location.href="subtopic_2/first.html"
+    }
+}
+divs=document.getElementsByClassName("three")
+for(let i=0; i<divs.length; i++){
+    divs[i].onclick=function(){
+        window.location.href="subtopic_3/first.html"
+    }
+}
+divs=document.getElementsByClassName("four")
+for(let i=0; i<divs.length; i++){
+    divs[i].onclick=function(){
+        window.location.href="subtopic_4/first.html"
+    }
+}
