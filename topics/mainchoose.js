0cfd92d004ad48e2906020aa9571174c672b8da0
FullMonkey <irondavide06@gmail.com>
2025-11-02 22:29:00 +0100

    back button subtopic setted up

--- topics/mainchoose.js
+++ topics/mainchoose.js
@@ -2,17 +2,21 @@ let divs=document.getElementsByClassName("bug")
 let temp;
 for(let i=0; i<divs.length; i++){
      divs[i].onclick=function(){
+        sessionStorage.setItem("category", "bug");
         temp=window.location.href.slice(0,-11) +"topicgeneral_b.html"
          window.location.href=temp
+
     }
 }
 
 divs=document.getElementsByClassName("vul")
 for(let i=0; i<divs.length; i++){
     divs[i].onclick=function(){
-             
+        sessionStorage.setItem("category", "vul"); 
+        console.log(sessionStorage.getItem("category"));
         temp=window.location.href.slice(0,-11) +"topicgeneral_v.html"
+        window.location.href=temp
+       
 
-         window.location.href=temp
     }
 }
--- topics/topicgeneral.js
+++ topics/topicgeneral.js
@@ -1,7 +1,16 @@
 divs=document.getElementsByClassName("exit")
 for(let i=0; i<divs.length; i++){
      divs[i].onclick=function(){
-         window.location.href="../topics.html"
+
+        if(sessionStorage.getItem("category")=="bug"){
+
+            temp=window.location.href.slice(0,-31)+"topicgeneral_b.html"
+            window.location.href=temp
+        }
+        else{
+             temp=window.location.href.slice(0,-31)+"topicgeneral_v.html"
+            window.location.href=temp
+        }
     }
 }
 
