f90cb21e25f4670a8b1a311cece4626ad0f05a30
FullMonkey <irondavide06@gmail.com>
2025-11-02 11:11:18 +0100

    scoll pinch

--- topics/topics.js
+++ topics/topics.js
@@ -22,3 +22,48 @@ document.getElementById("health").addEventListener("click", ()=>{
     window.location.href="topic_4/topic_4_general.html";
 });
 
+
+let idint= setInterval(() => scrl(), 3000);
+
+function scrl(){
+    window.scrollTo({
+    top: 150,      
+    left: 0,       
+    behavior: 'smooth' 
+    });
+    setTimeout(() => 
+    window.scrollTo({
+    top: 0,      
+    left: 0,       
+    behavior: 'smooth' 
+    }),200)
+    
+}
+
+let moving=false;
+
+document.addEventListener('mousemove', (event) => {
+    clearInterval(idint);
+});
+
+function onMouseStop(callback, delay = 2000) {
+  let timer;
+  document.addEventListener('mousemove', () => {
+    clearTimeout(timer);
+    timer = setTimeout(callback, delay);
+  });
+}
+
+onMouseStop(() => {
+    if(window.scrollY===0){
+    idint = setInterval(() => scrl(), 3000);
+    }
+}, 2000);
+
+
+
+
+
+
+
+
