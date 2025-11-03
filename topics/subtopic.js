f2a782adc8f4ec37b8b799c7291feeabd4b53bf7
FullMonkey <irondavide06@gmail.com>
2025-11-03 16:38:22 +0100

    fixed page ex 3

--- topics/subtopic.js
+++ topics/subtopic.js
@@ -10,87 +10,54 @@ function toggleMenu() {
 
 
 
-link=document.getElementsByClassName("1");
-link=document.getElementsByClassName("2");
-link=document.getElementsByClassName("3");
-link=document.getElementsByClassName("4");
-link=document.getElementsByClassName("5");
-
-var linkOne = false
-var linkTwo = false
-var linkThree = false
-var linkFour = false
-var linkFive = false
 
 
-for(let i=0; i<link.length; i++){
-    link[i].onclick=function(){
-        var divFull = document.getElementsByClassName('one');
-        for(let j = 0; j<divFull.length; j++){
-            if (!linkOne){
-                linkOne = true
-                divFull[j].style.display = 'block';}
-            else{
-            divFull[j].style.display = 'none';
-            linkOne = false
-            }  
-        }
+var link1 = { val:false};
+var link2 = { val:false};
+var link3 = { val:false};
+var link4 = { val:false};
+var link5 = { val:false};
+
+function makeVisible(id, linkval){
+    
+    let link=document.getElementById(id);
+    let class2="one";
+    switch(id){
+        case "n1":
+            class2="one";
+            break;
+        case "n2":
+            class2="two";
+            break;
+        case "n3":
+            class2="three";
+            break;
+        case "n4":
+            class2="four";
+            break;
+        case "n5":
+            class2="five";
+            break;
     }
+
+    link.addEventListener("click", function(event){
+    
+        if(!linkval.val){
+            linkval.val = true
+            document.getElementById(class2).style.display = "block";
+        }else{
+            linkval.val = false
+            document.getElementById(class2).style.display = "none";
+        }
+    })
 }
 
-for(let i=0; i<link.length; i++){
-    link[i].onclick=function(){
-        var divFull = document.getElementsByClassName('two');
-        for(let j = 0; j<divFull.length; j++){
-            if (!linkOne){
-                linkOne = true
-                divFull[j].style.display = 'block';}
-            else{
-            divFull[j].style.display = 'none';
-            linkOne = false
-            }  
-        }
-    }
-}
-for(let i=0; i<link.length; i++){
-    link[i].onclick=function(){
-        var divFull = document.getElementsByClassName('three');
-        for(let j = 0; j<divFull.length; j++){
-            if (!linkOne){
-                linkOne = true
-                divFull[j].style.display = 'block';}
-            else{
-            divFull[j].style.display = 'none';
-            linkOne = false
-            }  
-        }
-    }
-}
-for(let i=0; i<link.length; i++){
-    link[i].onclick=function(){
-        var divFull = document.getElementsByClassName('four');
-        for(let j = 0; j<divFull.length; j++){
-            if (!linkOne){
-                linkOne = true
-                divFull[j].style.display = 'block';}
-            else{
-            divFull[j].style.display = 'none';
-            linkOne = false
-            }  
-        }
-    }
-}
-for(let i=0; i<link.length; i++){
-    link[i].onclick=function(){
-        var divFull = document.getElementsByClassName('five');
-        for(let j = 0; j<divFull.length; j++){
-            if (!linkOne){
-                linkOne = true
-                divFull[j].style.display = 'block';}
-            else{
-            divFull[j].style.display = 'none';
-            linkOne = false
-            }  
-        }
-    }
-}
+
+
+makeVisible("n1", link1);
+makeVisible("n2", link2);
+makeVisible("n3", link3);
+makeVisible("n4", link4);
+makeVisible("n5", link5);
+
+
